4051c1d54b7aed4421d3557fd3c6d6ffbd6d7d54
PhanPhuocDat <1824801030046@tdmu.edu.vn>
2021-10-17 22:24:27 +0700

    change er

--- frontend/src/srceens/ProductScreen.js
+++ frontend/src/srceens/ProductScreen.js
@@ -5,6 +5,9 @@ const ProductScreen = {
   render: async () => {
     const request = parseRequestUrl();
     const product = await getProduct(request.id);
+    if(product.error) {
+      return `<div>${product.error}</div>`;
+    }
     return `<h1>${product.name}</h1>`
   }
 };
