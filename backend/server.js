99d0f053aef8e79f3c3a8e396cd1358ed4461e54
PhanPhuocDat <1824801030046@tdmu.edu.vn>
2021-10-23 13:38:32 +0700

    connect to Mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
+import config from './config';
 import data from './data';
+import userRouter from './routers/userRouter';
 
 mongoose.connect(config.MONGODB_URL,{
   useNewUrlParser :true,
   useUnifiedTopology:true,
   useCreateIndex: true,
-}).catch((error)=>{
+}).then(()=>{
+  console.log('Connected to mongodb')
+})
+.catch((error)=>{
   console.log(error.reason);
 })
 const app = express();
 app.use(cors());
+app.use('/api/users',userRouter)
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
